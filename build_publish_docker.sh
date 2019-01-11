721d11506b9dddb8f64ff3c7de9ef7f9979b3968
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2019-01-11 10:27:13 +0100

    [build][fix?] properly mark as latest

--- build_publish_docker.sh
+++ build_publish_docker.sh
@@ -10,7 +10,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build --build-arg CULPER_VER=${tag} -t ${tagged_image_name} .
 
-if ![[ $(echo $tag | grep -E "(alpha|beta|rc)") ]]
+if echo $tag | grep -E "(alpha|beta|rc)"
 then
     docker tag ${tagged_image_name} ${base_image_name}:latest
 fi
